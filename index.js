290ad3705af6133e13a0e2718f8980caff63a0cb
cardenalcopia <kirisse85@gmail.com>
2022-05-09 15:19:33 +0200

    actualizacion

---
+++ index.js
@@ -0,0 +1,22 @@
+function suma (op1,op2)
+{
+    let resultado = op1 + op2;
+    console.log( op1 + "+" + op2 + "=" + resultado);
+}
+
+function resta (op1,op2)
+{
+    let resultado = op1-op2;
+    console.log( op1 + "-" + op2 + "=" + resultado);
+}
+function producto (op1,op2)
+{
+    let resultado = op1*op2;
+    console.log( op1 + "*" + op2 + "=" + resultado);
+}
+function division (op1,op2)
+{
+    let resultado = op1/op2;
+    console.log( op1 + "/" + op2 + "=" + resultado);
+}
+module.exports={suma,resta,producto,division}
