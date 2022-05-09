a8c781ab53f4979e66811ce610875200ee970a34
cardenalcopia <kirisse85@gmail.com>
2022-05-09 15:22:32 +0200

    actualizacion

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function suma (op1,op2)
+/*function suma (op1,op2)
 {
     let resultado = op1 + op2;
     console.log( op1 + "+" + op2 + "=" + resultado);
-}
+}*/
 
 function resta (op1,op2)
 {
@@ -19,4 +19,4 @@ function division (op1,op2)
     let resultado = op1/op2;
     console.log( op1 + "/" + op2 + "=" + resultado);
 }
-module.exports={suma,resta,producto,division}
+module.exports={resta,producto,division}
--- run.js
+++ run.js
@@ -1,5 +1,5 @@
 let myLib = require("./index");
-myLib.suma(5,4);
+
 myLib.resta(5,4);
 myLib.producto(5,4);
 myLib.division(5,4);
