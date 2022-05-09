1c49fa1330534f7a5c5aebe65e0bdfde96e4f8fe
cardenalcopia <kirisse85@gmail.com>
2022-05-09 16:02:50 +0200

    subida

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
 /*function suma (op1,op2)
 {
     let resultado = op1 + op2;
     console.log( op1 + "+" + op2 + "=" + resultado);
 }*/
-=======
 function suma (op1,op2,op3)
 {
     let resultado = op1 + op2 + op3;
     console.log( op1 + "+" + op2 + "+" + op3 +"=" + resultado);
 }
->>>>>>> rama2
 
 function resta (op1,op2)
 {
@@ -27,8 +24,5 @@ function division (op1,op2)
     let resultado = op1/op2;
     console.log( op1 + "/" + op2 + "=" + resultado);
 }
-<<<<<<< HEAD
-module.exports={resta,producto,division}
-=======
+
 module.exports={suma,resta,producto,division}
->>>>>>> rama2
