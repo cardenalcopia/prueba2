02047086534f4627148b587071b88491f7e4f199
cardenalcopia <kirisse85@gmail.com>
2022-05-09 15:30:53 +0200

    subida

---
+++ index.js
@@ -0,0 +1,22 @@
+function suma (op1,op2,op3)
+{
+    let resultado = op1 + op2 + op3;
+    console.log( op1 + "+" + op2 + "+" + op3 +"=" + resultado);
+}
+
+function resta (op1,op2)
+{
+    let resultado = op1-op2;
+    console.log( op1 + "-" + op2 + "=" + resultado);
+}
+function producto (op1,op2)
+{
+    let resultado = op1*op2;
+    console.log( op1 +"*" + op2 + "=" + resultado);
+}
+function division (op1,op2)
+{
+    let resultado = op1/op2;
+    console.log( op1 + "/" + op2 + "=" + resultado);
+}
+module.exports={suma,resta,producto,division}
---
+++ run.js
@@ -0,0 +1,5 @@
+let myLib = require("./index");
+myLib.suma(3,2,1);
+myLib.resta(5,3);
+myLib.producto(5,4);
+myLib.division(5,4);
